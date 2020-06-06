5792c021a8747f75d5305c602af09c5adcbebd6e
Fadhil Kurnia <fadhilimamk@gmail.com>
2020-06-06 12:45:57 +0700

    Load data from data directory

--- app.py
+++ app.py
@@ -9,14 +9,27 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 load_dotenv()
 
+# reading mail template from data directory
+data_mail_template = ""
+with open('data/email_template.txt', 'r') as file:
+    data_mail_template = file.read()
+
+# reading mail subject from data directory
+data_mail_subject = ""
+with open('data/email_subject.txt', 'r') as file:
+    data_mail_subject = file.read().replace('\n', '')
+
 def send_mail(target_address, sender, subject):
     mail = Mail(app)
 
     msg = Message(subject, sender=sender, recipients=[target_address])
 
-    msg.body = ""
+    msg.body = data_mail_template
+    msg.body = msg.body.replace('{{EMAIL}}', target_address)
 
-    mail.send(msg)
+    print(msg.body)
+
+    # mail.send(msg)
     print("Message sent to " + target_address)
 
     return None
@@ -31,12 +44,17 @@ def send_to_all():
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
 
-    list_of_address = ["fadhilikurnia@gmail.com", "sukaberkhayal@gmail.com"]
-    mail_subject = ""
+    # load recepients from data directory
+    list_of_address = []
+    with open ("data/recepients.txt", "r") as myfile:
+        list_of_address=myfile.readlines()
+
+    sender = os.getenv("MAIL_USERNAME")
+    mail_subject = data_mail_subject
 
     try:
         for address in list_of_address:
-            send_mail(address)
+            send_mail(address, sender, mail_subject)
     finally:
         return "Please watch the server log!"
 
