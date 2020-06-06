d92b5303ef958e0de9f50ed78d4f464953923963
Fadhil Kurnia <fadhilimamk@gmail.com>
2020-06-06 12:37:14 +0700

    Move config in .env

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Flask app to send mail using Gmail SMTP server.
 To use,
 
 1. Install: pip3 install -r requirements.txt
-2. Configure gmail account by modifying MAIL_USERNAME & MAIL_PASSWORD  
-3. Configure recipients by modifying list_of_address in function send_to_all.
-4. flask run 
+2. Configure gmail account by modifying MAIL_USERNAME & MAIL_PASSWORD in `.env`. You can get the MAIL_PASSWORD from `https://myaccount.google.com/apppasswords`.
+3. Configure recipients by editing `data/recepients.txt`.
+4. Configure email subject and content in `data/email_subject.txt` & `data/email_template.txt`
+4. Run the flask server `python3 app.py`
 5. Start sending email: Open browser, localhost:5000/send_to_all 
--- app.py
+++ app.py
@@ -2,37 +2,19 @@ import os
 import sys
 import datetime
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
+load_dotenv()
 
-def send_mail(target_address):
+def send_mail(target_address, sender, subject):
     mail = Mail(app)
 
-    msg = Message("Akses Webinar 'Sekolah PhD itu Gratis?'",
-                sender="martinluttap@gmail.com",
-                recipients=[target_address])
+    msg = Message(subject, sender=sender, recipients=[target_address])
 
-    msg.body ="""
-Hi,
-
-Terimakasih sudah mendaftar untuk webinar "Sekolah PhD itu Gratis? Tanya Jawab Bersama Kakak-Kakak Mahasiswa PhD Indonesia di Amerika Serikat".
-Untuk mengakses webinar silahkan gunakan link berikut:
-
-Link Zoom:
-Link YouTube:
-
-Link YouTube Alternatif 1: 
-Link YouTube Altrenatif 2:
-
-Kami menerima 900 lebih pendaftaran, namun kami tidak bisa menampung semuanya dalam platform yang kami gunakan. Hanya 100 orang saja yang bisa masuk ke dalam room Zoom, sisanya dapat menyaksikan dan mengajukan pertanyaan di Live Youtube.
-
-Kami tunggu kehadiranmu!
-
-Terimakasih
--- Fadhil, Martin, Daniar, Bella, Titan
-"""
+    msg.body = ""
 
     mail.send(msg)
     print("Message sent to " + target_address)
@@ -41,15 +23,16 @@ Terimakasih
 
 @app.route('/send_to_all')
 def send_to_all():
-    # Send ticket to the address
-    app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'martinluttap@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'hbhftcdinzjmnapo'
+
+    app.config['MAIL_SERVER']= os.getenv("MAIL_SERVER")
+    app.config['MAIL_PORT'] = os.getenv("MAIL_PORT")
+    app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = os.getenv("MAIL_SERVER")
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
 
     list_of_address = ["fadhilikurnia@gmail.com", "sukaberkhayal@gmail.com"]
+    mail_subject = ""
 
     try:
         for address in list_of_address:
